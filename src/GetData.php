ef1d66831d0300be957ee78e16396081e38bf79d
Evaristo Carvalho <evaristo.colomby@gmail.com>
2015-09-25 14:45:54 +0100

    First Commit

--- src/GetData.php
+++ src/GetData.php
@@ -1,16 +1,30 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of GetData
  *
  * @author Evaristo
  */
 class GetData {
-  //put your code here
+  public function connectFunction($query){
+    $conn_string = "host=sheep port=5432 dbname=test user=lamb password=bar";
+    
+    $conn = pg_connect($conn_string);
+ 
+    if (!$conn) {
+      echo "An error occurred.\n";
+      exit;
+    }
+
+    $result = pg_query($conn, "SELECT author, email FROM authors");
+    if (!$result) {
+      echo "An error occurred.\n";
+      exit;
+    }
+
+    while ($row = pg_fetch_row($result)) {
+      echo "Author: $row[0]  E-mail: $row[1]";
+      echo "<br />\n";
+    }
+  }
 }

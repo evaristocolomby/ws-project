3449b4d0e0a69065c11b56cb48c35e9d23899fe2
Evaristo Carvalho <evaristo.colomby@gmail.com>
2015-10-23 13:18:20 +0100

    Improve security

--- GetData.php
+++ GetData.php
@@ -1,4 +1,9 @@
 <?php
+require_once 'DetectInjection.php';
+
+$get = testArray($_GET);
+$post = testArray($_POST);
+$cookie = testArray($_COOKIE);
 
 $query = $_GET['query'];
 
@@ -14,6 +19,17 @@ if (!$conn) {
 	echo "An error occurred.\n";
     exit;
 }
+
+if ($get > 0 || $post > 0 || $cookie > 0){
+  $data[] = 'SQL Attack attempt detected';
+  $data[] = 'Aborting operation';
+  //var_dump($get); var_dump($post); var_dump($cookie);
+} else {
+preg_match('/(limit)\s\d(((\,\s)|(\s\,\s)|(\s\,))\d)?/i', $query, $matches);  //1
+
+if (!$matches){
+  $query = $query . ' limit 1000';
+}
 $result = pg_query($conn, $query);
 
 $data = array();
@@ -21,11 +37,13 @@ $data = array();
 while ($row = pg_fetch_row($result)) {
        $data[] = $row;
 }
-
+}
 //file_put_contents($logfile, '\n data lenght: ' . count($data), FILE_APPEND | LOCK_EX);
 
 //$data = [$query];
 
 header('Content-type: application/json');
+
 echo json_encode(array('result'=>$data));
+
 ?>
